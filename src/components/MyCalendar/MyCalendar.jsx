6a91a822fcaf75d82e89311b77686201f08ed366
Melani Alassia <alassiamelani1@gmail.com>
2024-07-24 17:49:27 -0300

    modificacion de calendario

--- src/components/MyCalendar/MyCalendar.jsx
+++ src/components/MyCalendar/MyCalendar.jsx
@@ -1,34 +1,35 @@
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import dayjs from "dayjs";
-import 'dayjs/locale/es';
-dayjs.locale('es'); //traduccion al español
+import "dayjs/locale/es";
+dayjs.locale("es"); //traduccion al español
 //STYLE
-import style from "./MyCalendar.module.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./mycalendar.css";
 
 const MyCalendar = () => {
   const localizer = dayjsLocalizer(dayjs);
   return (
-    <div className={style.container}>
+    <div className="flex p-4 w-full h-full">
       <Calendar
         localizer={localizer}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 500 }}
+        style={{ height: 700, width: 900, margin: "0 auto" }}
+        className="custom"
         views={["month", "week", "day"]}
         messages={{
-          allDay: 'Todo el día',
-          previous: 'Anterior',
-          next: 'Siguiente',
-          today: 'Hoy',
-          month: 'Mes',
-          week: 'Semana',
-          day: 'Día',
-          agenda: 'Agenda',
-          date: 'Fecha',
-          time: 'Hora',
-          event: 'Evento',
-          noEventsInRange: 'No hay eventos',
+          allDay: "Todo el día",
+          previous: "Anterior",
+          next: "Siguiente",
+          today: "Hoy",
+          month: "Mes",
+          week: "Semana",
+          day: "Día",
+          agenda: "Agenda",
+          date: "Fecha",
+          time: "Hora",
+          event: "Evento",
+          noEventsInRange: "No hay eventos",
           showMore: (total) => `+${total} más`,
         }}
       />
