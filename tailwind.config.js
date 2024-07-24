493e1d8d1a0c3cdeeb5425a444b009b9d60524e6
Melani Alassia <alassiamelani1@gmail.com>
2024-07-24 17:49:47 -0300

    se agrego un nuevo color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-transparent': 'rgba(192, 192, 192, 0.5)' 
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
