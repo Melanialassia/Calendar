55ae523b88c6315bc0aa972fece6ec5761c5d28e
Melani Alassia <alassiamelani1@gmail.com>
2024-07-25 19:46:10 -0300

    Renderizacion y solicitud de turnos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
 //HOOKS
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //COMPONENTS
 import CreateEvent from "./components/CreateEvent/CreateEvent";
 import LateralMenu from "./components/LateralMenu/LateralMenu";
 import MyCalendar from "./components/MyCalendar/MyCalendar";
 import Navbar from "./components/Navbar/Navbar";
 import Modal from "./components/Modal/Modal";
+//STORE
+import { store } from "./store/store";
 
 function App() {
+  const { getEvents, events } = store();
 
-  const[showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    if(events.length === 0) {
+      getEvents();
+    }
+  }, []);
 
+
+  const [showModal, setShowModal] = useState(false);
+  
   const openModal = () => {
-    setShowModal(true)
-  }
+    setShowModal(true);
+  };
 
   const closeModal = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
+
   return (
     <div className="m-0 p-0">
-      <Navbar  openModal={openModal}/>
-      <div className="max-w-screen-xl flex flex-row justify-between mx-auto p-4">
+      <Navbar openModal={openModal} />
+      <div className=" flex flex-row justify-between mx-auto p-4">
         <LateralMenu />
         <MyCalendar />
-      <Modal  show={showModal} onClose={closeModal}>
-        <CreateEvent />
-      </Modal>
+        <Modal show={showModal} onClose={closeModal}>
+          <CreateEvent />
+        </Modal>
       </div>
     </div>
   );
--- src/components/MyCalendar/MyCalendar.jsx
+++ src/components/MyCalendar/MyCalendar.jsx
@@ -2,25 +2,48 @@ import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import dayjs from "dayjs";
 import "dayjs/locale/es";
 dayjs.locale("es"); //traduccion al español
+//STORE
+import { store } from "../../store/store";
 //STYLE
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./mycalendar.css";
 
 const MyCalendar = () => {
+  const { events } = store();
+
+  const mapEvents = events.map((e) => ({
+    id: e.id,
+    title: e.title,
+    start: e.start,
+    end: e.end,
+  }));
+
+  console.log("hola", mapEvents);
+ //para poder agregarle estilos al calendario
+  const eventPropGetter = (e) => {
+    const style = {
+      background: e.id === -1 ? "#057A55" : "#6BBEE8",
+      color: "white",
+      border: "none",
+    };
+
+    return { style };
+  };
+
   const localizer = dayjsLocalizer(dayjs);
+
   return (
     <div className="flex p-4 w-full h-full">
       <Calendar
         localizer={localizer}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 700, width: '100%', margin: "0 auto" }}
+        events={mapEvents}
+        style={{ height: 700, width: "100%", margin: "0 auto" }}
+        eventPropGetter={eventPropGetter}
         className="custom"
-        views={["month", "week", "day"]}
+        views={["month", "week", "day", "agenda"]}
         messages={{
-          allDay: "Todo el día",
-          previous: "Anterior",
-          next: "Siguiente",
+          previous: "<",
+          next: ">",
           today: "Hoy",
           month: "Mes",
           week: "Semana",
