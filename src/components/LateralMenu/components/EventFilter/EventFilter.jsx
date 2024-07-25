41571430e7e787c9b58940141e48d360970323c0
Melani Alassia <alassiamelani1@gmail.com>
2024-07-25 19:47:03 -0300

    modifcacion de estilos e inputs

--- src/components/LateralMenu/components/EventFilter/EventFilter.jsx
+++ src/components/LateralMenu/components/EventFilter/EventFilter.jsx
@@ -11,7 +11,7 @@ const EventFilter = () => {
     <div>
       <div className="flex mt-5 transition-all duration-300 rounded hover:bg-gray-100">
       <button onClick={handleClose} className="flex justify-between items-center space-x-2 p-2 border-none h-full w-full">
-          <p className="font-bold text-lg text-center text-zinc-700">Mi calendario</p>
+          <p className="font-bold text-lg text-center text-zinc-700">Turnos</p>
           <img
             src={open ? "/logo/up.png" : "/logo/down.png"}
             alt="abrir"
@@ -23,16 +23,12 @@ const EventFilter = () => {
        <div className="mt-1 p-4 border-hidden rounded shadow-inset-custom bg-white ">
        <ul className="flex flex-col justify-center items-start">
          <li className="mt-1">
-           <input type="checkbox" id="mañana" className="mr-4 text-blue-600 border-2 border-blue-600 focus:ring-blue-600" />
-           <label htmlFor="mañana" className="text-zinc-700">Mañana</label>
+           <input type="checkbox" id="mañana" className="mr-4 text-green-600 border-2 border-green-600 focus:ring-green-600 " />
+           <label htmlFor="mañana" className="text-zinc-700">Disponible</label>
          </li>
          <li className=" mt-1">
-           <input type="checkbox" id="mediodia" className="mr-4 text-green-600 border-2 border-green-600 focus:ring-green-600 " />
-           <label htmlFor="mediodia" className="text-zinc-700">Mediodía</label>
-         </li>
-         <li className=" mt-1">
-           <input type="checkbox" id="tarde" className="mr-4 text-orange-600 border-2 border-orange-600 focus:ring-orange-600" />
-           <label htmlFor="tarde" className="text-zinc-700">Tarde</label>
+           <input type="checkbox" id="mediodia" className="mr-4 text-blue-500 border-2 border-blue-500 focus:ring-blue-500 " />
+           <label htmlFor="mediodia" className="text-zinc-700">Ocupados</label>
          </li>
        </ul>
      </div>
