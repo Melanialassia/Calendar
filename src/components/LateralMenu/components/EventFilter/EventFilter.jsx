bc797830a9faf9b770505295f7b60209249bf9c8
Melani Alassia <alassiamelani1@gmail.com>
2024-07-24 23:55:53 -0300

    estilos

--- src/components/LateralMenu/components/EventFilter/EventFilter.jsx
+++ src/components/LateralMenu/components/EventFilter/EventFilter.jsx
@@ -21,18 +21,18 @@ const EventFilter = () => {
       </div>
       {open && (
        <div className="mt-1 p-4 border-hidden rounded shadow-inset-custom bg-white ">
-       <ul>
-         <li className="flex items-center">
-           <input type="checkbox" id="mañana" className="mr-2" />
-           <label htmlFor="mañana">Mañana</label>
+       <ul className="flex flex-col justify-center items-start">
+         <li className="mt-1">
+           <input type="checkbox" id="mañana" className="mr-4 text-blue-600 border-2 border-blue-600 focus:ring-blue-600" />
+           <label htmlFor="mañana" className="text-zinc-700">Mañana</label>
          </li>
-         <li className="flex items-center mt-1">
-           <input type="checkbox" id="mediodia" className="mr-2" />
-           <label htmlFor="mediodia">Mediodía</label>
+         <li className=" mt-1">
+           <input type="checkbox" id="mediodia" className="mr-4 text-green-600 border-2 border-green-600 focus:ring-green-600 " />
+           <label htmlFor="mediodia" className="text-zinc-700">Mediodía</label>
          </li>
-         <li className="flex items-center mt-1">
-           <input type="checkbox" id="tarde" className="mr-2" />
-           <label htmlFor="tarde">Tarde</label>
+         <li className=" mt-1">
+           <input type="checkbox" id="tarde" className="mr-4 text-orange-600 border-2 border-orange-600 focus:ring-orange-600" />
+           <label htmlFor="tarde" className="text-zinc-700">Tarde</label>
          </li>
        </ul>
      </div>
