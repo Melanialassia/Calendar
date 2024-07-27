6bf2df4369fbda0592f08d69615198ae6d13d0ee
Melani Alassia <alassiamelani1@gmail.com>
2024-07-27 19:52:47 -0300

    se creo la funcion para crear eventos nuevos y se unifico las funciones getEvents y setEvents

--- src/store/store.js
+++ src/store/store.js
@@ -4,58 +4,119 @@ import dayjs from "dayjs";
 
 export const store = create((set, get) => ({
   events: [],
-  isLoaded: false,
   copyEvents: [],
+
+  // getEvents: async () => {
+  //   try {
+  //     console.log("aqui");
+  //     const { data } = await axios(
+  //       "https://my-json-server.typicode.com/juanpernu/bilog-fe-challenge/schedule"
+  //     );
+  //     const formatEvent = data.map((e) => {
+  //       const fecha = e.fecha.slice(0, 10);
+  //       const start = new Date(`${fecha}T${e.hora}:00`);
+
+  //       const end = new Date(start.getTime() + 30 * 60000); // a la hora del inicio  se le suma 30 minutos para asi generar la hora final
+
+  //       const result = {
+  //         id: e.id_agenda,
+  //         title: e.id_agenda === -1 ? "Disponible" : e.ape_nom,
+  //         start: start,
+  //         end: end,
+  //       };
+
+  //       return result;
+  //     });
+
+  //     localStorage.setItem("event", JSON.stringify(formatEvent));
+  //     localStorage.setItem("loaded", JSON.stringify(true));
+  //     console.log("aqui*2");
+
+  //     set((state) => ({
+  //       ...state,
+  //       events: formatEvent,
+  //       copyEvents: [...formatEvent],
+  //     }));
+  //   } catch (error) {
+  //     console.log("error", error);
+  //   }
+  // },
+
+  // setEvents: () => {
+  //   const copy = JSON.parse(localStorage.getItem("event"));
+
+  //   const result = copy.map((e) => ({
+  //     id: e.id,
+  //     title: e.title,
+  //     start: new Date(e.start),
+  //     end: new Date(e.end),
+  //   }));
+  //   console.log("entrealstore", result);
+  //   set({
+  //     copyEvents: result,
+  //   });
+  // },
 
   getEvents: async () => {
     try {
-      console.log("aqui");
-      const { data } = await axios(
-        "https://my-json-server.typicode.com/juanpernu/bilog-fe-challenge/schedule"
-      );
-      const formatEvent = data.map((e) => {
-        const fecha = e.fecha.slice(0, 10);
-        const start = new Date(`${fecha}T${e.hora}:00`);
+      const storedEvent = JSON.parse(localStorage.getItem("event"));
 
-        const end = new Date(start.getTime() + 30 * 60000); // a la hora del inicio  se le suma 30 minutos para asi generar la hora final
+      if (storedEvent) {
+        const events = JSON.parse(localStorage.getItem("event"));
+        const result = events.map((e) => ({
+          id: e.id,
+          title: e.title,
+          start: new Date(e.start),
+          end: new Date(e.end),
+        }));
+        set({
+          copyEvents: result,
+        });
+      } else {
+        console.log("entre");
+        const { data } = await axios(
+          "https://my-json-server.typicode.com/juanpernu/bilog-fe-challenge/schedule"
+        );
 
-        const result = {
-          id: e.id_agenda,
-          title: e.id_agenda === -1 ? "Disponible" : e.ape_nom,
-          start: start,
-          end: end,
-        };
+        const formatEvent = data.map((e) => {
+          const fecha = e.fecha.slice(0, 10);
+          const start = new Date(`${fecha}T${e.hora}:00`);
 
-        return result;
-      });
+          const end = new Date(start.getTime() + 30 * 60000); // a la hora del inicio  se le suma 30 minutos para asi generar la hora final
 
-      localStorage.setItem("event", JSON.stringify(formatEvent));
-      localStorage.setItem("loaded", JSON.stringify(true));
-      console.log("aqui*2");
+          const result = {
+            id: e.id_agenda,
+            title: e.id_agenda === -1 ? "Disponible" : e.ape_nom,
+            start: start,
+            end: end,
+          };
 
-      set((state) => ({
-        ...state,
-        events: formatEvent,
-      }));
+          return result;
+        });
+
+        localStorage.setItem("event", JSON.stringify(formatEvent));
+
+        set((state) => ({
+          ...state,
+          events: formatEvent,
+          copyEvents: [...formatEvent],
+        }));
+      }
     } catch (error) {
-      console.log("error", error);
+      console.log(error);
     }
   },
 
-  setEvents: () => {
-    const copy = JSON.parse(localStorage.getItem("event"));
-
-    const result = copy.map((e) => ({
-      id: e.id,
-      title: e.title,
-      start: new Date(e.start),
-      end: new Date(e.end),
-    }));
-
-    console.log("copy", copy);
-    set((state) => ({
-      ...state,
-      events: result,
-    }));
+  addEvent: (event) => {
+    set((state) => {
+      const updateEvents = [...state.copyEvents, event];
+      localStorage.setItem("event", JSON.stringify(updateEvents));
+      return {
+        ...state,
+        copyEvents: updateEvents,
+      };
+    });
   },
+
+  removeEvent: () => {},
 }));
