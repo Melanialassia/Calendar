1e676be8b0fef557d8114388740ff00785b74790
Melani Alassia <alassiamelani1@gmail.com>
2024-07-28 17:48:02 -0300

    Se elimino los comentarios. Se creo la funcion para editar y eliminar un evento y guardarlo en el localStorage.

--- src/store/store.js
+++ src/store/store.js
@@ -6,74 +6,22 @@ export const store = create((set, get) => ({
   events: [],
   copyEvents: [],
 
-  // getEvents: async () => {
-  //   try {
-  //     console.log("aqui");
-  //     const { data } = await axios(
-  //       "https://my-json-server.typicode.com/juanpernu/bilog-fe-challenge/schedule"
-  //     );
-  //     const formatEvent = data.map((e) => {
-  //       const fecha = e.fecha.slice(0, 10);
-  //       const start = new Date(`${fecha}T${e.hora}:00`);
-
-  //       const end = new Date(start.getTime() + 30 * 60000); // a la hora del inicio  se le suma 30 minutos para asi generar la hora final
-
-  //       const result = {
-  //         id: e.id_agenda,
-  //         title: e.id_agenda === -1 ? "Disponible" : e.ape_nom,
-  //         start: start,
-  //         end: end,
-  //       };
-
-  //       return result;
-  //     });
-
-  //     localStorage.setItem("event", JSON.stringify(formatEvent));
-  //     localStorage.setItem("loaded", JSON.stringify(true));
-  //     console.log("aqui*2");
-
-  //     set((state) => ({
-  //       ...state,
-  //       events: formatEvent,
-  //       copyEvents: [...formatEvent],
-  //     }));
-  //   } catch (error) {
-  //     console.log("error", error);
-  //   }
-  // },
-
-  // setEvents: () => {
-  //   const copy = JSON.parse(localStorage.getItem("event"));
-
-  //   const result = copy.map((e) => ({
-  //     id: e.id,
-  //     title: e.title,
-  //     start: new Date(e.start),
-  //     end: new Date(e.end),
-  //   }));
-  //   console.log("entrealstore", result);
-  //   set({
-  //     copyEvents: result,
-  //   });
-  // },
-
   getEvents: async () => {
     try {
       const storedEvent = JSON.parse(localStorage.getItem("event"));
 
       if (storedEvent) {
-        const events = JSON.parse(localStorage.getItem("event"));
-        const result = events.map((e) => ({
+        const result = storedEvent.map((e) => ({
           id: e.id,
           title: e.title,
           start: new Date(e.start),
           end: new Date(e.end),
         }));
+
         set({
           copyEvents: result,
         });
       } else {
-        console.log("entre");
         const { data } = await axios(
           "https://my-json-server.typicode.com/juanpernu/bilog-fe-challenge/schedule"
         );
@@ -86,7 +34,7 @@ export const store = create((set, get) => ({
 
           const result = {
             id: e.id_agenda,
-            title: e.id_agenda === -1 ? "Disponible" : e.ape_nom,
+            title: e.id_agenda === -1 ? "DISPONIBLE" : e.ape_nom,
             start: start,
             end: end,
           };
@@ -109,14 +57,45 @@ export const store = create((set, get) => ({
 
   addEvent: (event) => {
     set((state) => {
-      const updateEvents = [...state.copyEvents, event];
-      localStorage.setItem("event", JSON.stringify(updateEvents));
+      const addEvent = [...state.copyEvents, event];
+
+      localStorage.setItem("event", JSON.stringify(addEvent));
+
       return {
         ...state,
-        copyEvents: updateEvents,
+        copyEvents: addEvent,
       };
     });
   },
 
-  removeEvent: () => {},
+  updateEvents: (event) => {
+    set((state) => {
+      const filterEvent = state.copyEvents.filter(
+        (e) => e.start != event.start
+      );
+
+      const newListEvents = [...filterEvent, event];
+      localStorage.setItem("event", JSON.stringify(newListEvents));
+
+      return {
+        ...state,
+        copyEvents: newListEvents,
+      };
+    });
+  },
+
+  removeEvent: (event) => {
+    set((state) => {
+      const deleteEvent = state.copyEvents.filter(
+        (e) => e.start !== event.start
+      );
+
+      localStorage.setItem("event", JSON.stringify(deleteEvent));
+
+      return {
+        ...state,
+        copyEvents: deleteEvent,
+      };
+    });
+  },
 }));
