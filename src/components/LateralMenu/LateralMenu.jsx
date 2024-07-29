0cf982b68ccd340216c654e2ef38fae7eaaeb2ff
Melani Alassia <alassiamelani1@gmail.com>
2024-07-29 15:11:11 -0300

    Se volvio agregar el componente LatelMenu y se le agrego una nueva funcionalidad: - Al seleccionar una fecha especifica en este componente, esta misma se va a renderizan en el componente MyCalendar.

---
+++ src/components/LateralMenu/LateralMenu.jsx
@@ -0,0 +1,78 @@
+import { useEffect } from "react";
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+dayjs.locale("es"); //traduccion al español
+//STORE
+import { store } from "../../store/store";
+//STYLE
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./lateralmenu.css";
+import EventFilter from "./components/EventFilter/EventFilter";
+
+const LateralMenu = ({ setDateSelected }) => {
+  const localizer = dayjsLocalizer(dayjs);
+  const { getEvents, events } = store();
+
+  useEffect(() => {
+    getEvents();
+  }, [getEvents]);
+
+  const styleEvents = () => {
+    const style = {
+      background: "transparent",
+      color: "transparent",
+      height: "10px",
+      with: "10px",
+      marginTop: "-10px",
+    };
+
+    return { style };
+  };
+
+  const handleDateSelected = (date) => {
+    const newDate = date.start 
+    setDateSelected(newDate)
+  } 
+
+  return (
+    <aside className="flex flex-row p-4">
+      <ul>
+        <li>
+          <Calendar
+            localizer={localizer}
+            startAccessor="start"
+            events={events}
+            defaultDate={new Date(2021, 8, 16)}
+            onSelectSlot={handleDateSelected}
+            selectable
+            style={{ height: 300, width: "100%" }}
+            eventPropGetter={styleEvents}
+            className="custom-calendar"
+            views={["month"]}
+            messages={{
+              previous: (
+                <img src="/logo/left.png" alt="izquierda" className="w-5 h-5" />
+              ),
+              next: (
+                <img src="/logo/right.png" alt="derecha" className="w-5 h-5" />
+              ),
+              today: <p className="w-7 h-5">Hoy</p>,
+              agenda: "Agenda",
+              date: "Fecha",
+              time: "Hora",
+              event: "Evento",
+              noEventsInRange: "No hay eventos",
+              showMore: (total) => `+${total}`,
+            }}
+          />
+        </li>
+        <li>
+          <EventFilter />
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default LateralMenu;
