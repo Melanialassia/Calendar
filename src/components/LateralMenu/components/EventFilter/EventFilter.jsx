b77cf01c5f65d1f04e3393f938255987369806d6
Melani Alassia <alassiamelani1@gmail.com>
2024-07-29 17:34:19 -0300

    Se modifico los checkbox por etiquetas select

--- src/components/LateralMenu/components/EventFilter/EventFilter.jsx
+++ src/components/LateralMenu/components/EventFilter/EventFilter.jsx
@@ -1,38 +1,24 @@
-import { useState } from "react";
+//STORE
+import { store } from "../../../../store/store";
 
-const EventFilter = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleClose = () => {
-    setOpen(!open);
-  };
+const EventFilter = ({ handleFilterChange }) => {
+  const { filterEvent } = store();
 
   return (
     <div>
-      <div className="flex mt-5 transition-all duration-300 rounded hover:bg-gray-100">
-      <button onClick={handleClose} className="flex justify-between items-center space-x-2 p-2 border-none h-full w-full">
-          <p className="font-bold text-lg text-center text-zinc-700">Turnos</p>
-          <img
-            src={open ? "/logo/up.png" : "/logo/down.png"}
-            alt="abrir"
-            className="w-4"
-          />
-        </button>
+      <div className="flex mt-5 ">
+        <h2 className="font-bold text-lg text-center text-zinc-700">Turnos</h2>
       </div>
-      {open && (
-       <div className="mt-1 p-4 border-hidden rounded shadow-inset-custom bg-white ">
-       <ul className="flex flex-col justify-center items-start">
-         <li className="mt-1">
-           <input type="checkbox" id="mañana" className="mr-4 text-green-600 border-2 border-green-600 focus:ring-green-600 " />
-           <label htmlFor="mañana" className="text-zinc-700">Disponible</label>
-         </li>
-         <li className=" mt-1">
-           <input type="checkbox" id="mediodia" className="mr-4 text-blue-500 border-2 border-blue-500 focus:ring-blue-500 " />
-           <label htmlFor="mediodia" className="text-zinc-700">Ocupados</label>
-         </li>
-       </ul>
-     </div>
-      )}
+      <div className="mt-1 p-4 border-hidden rounded shadow-inset-custom bg-white ">
+        <select
+          onChange={(event) =>handleFilterChange(event)}
+          className="block w-full mt-1 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        >
+          <option value="todos">Todos</option>
+          <option value="disponible">Disponible</option>
+          <option value="ocupado">Ocupado</option>
+        </select>
+      </div>
     </div>
   );
 };
