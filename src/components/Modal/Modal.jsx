0b5758a8621d0fe50787b145029175b69c30d861
Melani Alassia <alassiamelani1@gmail.com>
2024-07-29 21:41:25 -0300

    estilos

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,8 +3,8 @@ const Modal = ({ show, closeModal, children }) => {
   if (!show) return null;
 
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 mx-auto h-full w-full flex justify-center items-center bg-gray-transparent z-50">
-      <div className=" relative rounded-lg w-400 h-400 p-4 bg-white">
+    <div className="fixed top-0 left-0 right-0 bottom-0 h-full w-full flex justify-center items-center bg-gray-transparent z-50">
+      <div className=" relative rounded-lg w-400 max-w-lg mt-14 p-4 bg-white">
         <button
           onClick={closeModal}
           className=" absolute p-2.5 right-3 rounded-lg transition-all duration-300 hover:bg-gray-100"
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = ({ openCreateModal }) => {
   return (
     <nav className="bg-white">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="/"
           className="flex items-center space-x-3 text-zinc-700 rtl:space-x-reverse"
@@ -11,7 +11,6 @@ const Navbar = ({ openCreateModal }) => {
             Calendario
           </span>
         </a>
-        {/* BARDA DE BUSQUEDA */}
         <div className="flex md:order-2">
           <button
             onClick={openCreateModal}
